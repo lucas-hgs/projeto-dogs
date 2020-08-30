749dcba12b0875839639e018730eb42d5d86cda9
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2020-08-30 19:32:12 -0300

    Feito o hook de useFetch

--- src/pages/LoginCreate/index.js
+++ src/pages/LoginCreate/index.js
@@ -4,8 +4,10 @@ import { Container } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import Error from '../../components/Error';
 
 import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
 
 import { UserContext } from '../../contexts/UserContext';
 
@@ -18,6 +20,8 @@ const LoginCreate = () => {
 
   const { userLogin } = useContext(UserContext);
 
+  const { loading, error, request } = useFetch();
+
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = USER_POST({
@@ -25,7 +29,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     })
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if(response.ok) userLogin(username.value, password.value);
   }
 
@@ -36,7 +40,8 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? <Button disabled>Cadastrando</Button> : <Button>Cadastrar</Button>}
+        <Error error={error} />
       </form>
     </Container>
   )
